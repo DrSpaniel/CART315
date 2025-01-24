6329a9572b83f4081000bb35754dfb0f60cbacc5
Daniel G <80426387+DrSpaniel@users.noreply.github.com>
2025-01-23 21:03:01 -0500

    Implement movement controls and collision logging in CollectorScript

--- Projects/_fallingAsleep/Assets/CollectorScript.cs
+++ Projects/_fallingAsleep/Assets/CollectorScript.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class CollectorScript : MonoBehaviour
 {
+
+    public float xLoc, yLoc;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        xLoc = 0;
+        yLoc = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Z)){
+            Debug.Log("Left!");
+            xLoc -= 0.1f;
+        }
+        if (Input.GetKey(KeyCode.X)){
+            Debug.Log("Right!");
+            xLoc += 0.1f;
+        }
+        this.transform.position = new Vector3(xLoc, yLoc, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D other){
+        Debug.Log(other.gameObject.name);
     }
 }
